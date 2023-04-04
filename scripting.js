e09150c972007ba10c372d5f6fe2aed581861246
kevin <kevin.madrigali@stud.unifi.it>
2023-04-05 01:26:59 +0200

    Changed a lot of things. First big commit

--- scripting.js
+++ scripting.js
@@ -1,11 +1,20 @@
 const w = window.innerWidth;
-const targetWidth = 1024;
-if ( w > targetWidth) {
-}
-else {
-    document.getElementById("section").textContent="";
+const smallScreenWidth = 1024;
+const tabletsWidth = 768;
+const mediumScreenWidth = 1200;
+if ( w <= smallScreenWidth) {
+    document.getElementById("4_theme").style.display = 'none';
     const sections = document.getElementById('sections');
     sections.innerHTML = "";
+}
+
+if ( w <= tabletsWidth) {
+    document.getElementById("3_theme").style.display = 'none';
+    document.getElementById("video3").style.display = 'none';
+}
+
+if ( w <= mediumScreenWidth) {
+    document.getElementById("video4").style.display = 'none';
 }
 
 
@@ -13,8 +22,24 @@ window.addEventListener("resize", function() {
     if (window.matchMedia("(min-width: 1024px)").matches) {
         const sections = document.getElementById('sections');
         sections.innerHTML = "Sezioni";
+        document.getElementById("4_theme").style.display = 'initial';
     } else {
         const sections = document.getElementById('sections');
         sections.innerHTML = "";
+        document.getElementById("4_theme").style.display = 'none';
+    }
+
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        document.getElementById("3_theme").style.display = 'initial';
+        document.getElementById("video3").style.display = 'flex';
+    } else {
+        document.getElementById("3_theme").style.display = 'none';
+        document.getElementById("video3").style.display = 'none';
+    }
+
+    if (window.matchMedia("(min-width: 1200px)").matches) {
+        document.getElementById("video4").style.display = 'flex';
+    } else {
+        document.getElementById("video4").style.display = 'none';
     }
 })
