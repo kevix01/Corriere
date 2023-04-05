b0a00e5688bf60ead2db6e012ba292a580584019
kevin <kevin.madrigali@stud.unifi.it>
2023-04-05 15:58:25 +0200

    Added weather to navbar with animation. Added <a> tags where needed.

--- scripting.js
+++ scripting.js
@@ -43,3 +43,37 @@ window.addEventListener("resize", function() {
         document.getElementById("video4").style.display = 'none';
     }
 })
+
+window.addEventListener('scroll', function() {
+    const element = document.getElementById('corriere_big_logo')
+    const position = element.getBoundingClientRect();
+
+    // checking whether fully visible
+    /*if(position.top >= 0 && position.bottom <= window.innerHeight) {
+        console.log('Element is fully visible in screen');
+    }*/
+
+    // checking for partial visibility
+    if(position.top < window.innerHeight && position.bottom - vh(10.5) >= 0) {
+        document.getElementById("logo").style.display = 'none';
+        document.getElementById("weather").style.display = 'flex';
+
+    }
+
+    // checking for not visibility
+    if(position.top < window.innerHeight && position.bottom - vh(10.5) < 0) {
+        console.log('Element is partially visible in screen');
+        document.getElementById("weather").style.display = 'none';
+        document.getElementById("logo").style.display = 'initial';
+    }
+});
+
+function vh(percent) {
+    const h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (percent * h) / 100;
+}
+
+function vw(percent) {
+    const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    return (percent * w) / 100;
+}
